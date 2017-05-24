11f3b9e219edfac5ba1fc51e6b604e2689f3db16
Michael Murphy <mikeymurph77@gmail.com>
2017-05-23 23:22:29 -0400

    Add homepage view and route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', storeController.homepage);
 
 module.exports = router;
