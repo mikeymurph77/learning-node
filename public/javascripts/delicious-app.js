f330851390d530428c33119868e2415c4bf4069a
Michael Murphy <mikeymurph77@gmail.com>
2017-06-12 22:33:05 -0400

    Add google autocomplete to store form

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,6 @@
 import '../sass/style.scss';
 
-import { $, $$ } from './modules/bling';
+import { $, $$ } from './modules/bling'; // Allows jQuery like syntax
+import autocomplete from './modules/autocomplete';
+
+autocomplete( $('#address'), $('#lat'), $('#lng') );
